75e687245c5e0f7e5fa2fd3f645c13af45fb9877
Shobana Hariraman <shobanaravi2004@gmail.com>
2025-02-14 20:44:22 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,34 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import TaskUpdate from "./components/TaskUpdate";
 import CheckBoxFn from "./components/CheckBoxFn";
 import "./App.css";
 
-function App() {  
-
-  const initialState = { initialInput: "", tasks: []};
+function App() {
+  const initialState = { initialInput: "", tasks: [] };
   const [state, dispatch] = useReducer(TaskUpdate, initialState);
-  // const [state2,dispatch2] = useReducer(EditTask,"false");
 
   function handleChange(e) {
     dispatch({ type: "input", payload: e.target.value });
   }
 
   function handleCheckbox(index) {
-    console.log("Index:"+index);
-    
-    //state.tasks[index].isChecked = !state.tasks[index].isChecked;
-    
-    // console.log('state.tasks[e.target.name].isChecked'+state.isChecked);
-    dispatch({type: "toggle-task", payload:index});
-    //state.isChecked
+    dispatch({ type: "toggle-task", payload: index });
   }
 
-  function handleEditTask(e){
-    console.log('e.target.value:'+e.target.value)
-    dispatch({type:"edit-task"})
+  function handleEditTask(index, e) {
+    dispatch({
+      type: "edit-task",
+      payload: { value: e.target.value, index: index },
+    });
   }
   return (
     <>
       <h1>To-Do-List</h1>
       <input type="text" value={state.initialInput} onChange={handleChange} />
-      
+
       <button
         onClick={() => {
-          dispatch({ type: "add"});
+          dispatch({ type: "add" });
         }}
       >
         Add Task
@@ -42,14 +36,29 @@ function App() {
       <ul>
         {state.tasks.map((task, index) => (
           <li key={index}>
-                    <input type="text" name = "txbTask" value={task.text} disabled={task.isEditClicked} onChange={handleEditTask} />
-                    <button onClick={() => dispatch({type: "edit",payload:index})}>Edit</button>
-                    <button onClick={() => dispatch({type: "delete"})} disabled={!task.isChecked}>Delete</button>
-                    <input type = "checkbox" name ={index}  onChange={() => handleCheckbox(index)}></input>
+            <input
+              type="text"
+              name="txbTask"
+              value={task.text}
+              disabled={task.isEditClicked}
+              onChange={(e) => handleEditTask(index, e)}
+            />
+            <button onClick={() => dispatch({ type: "edit", payload: index })}>
+              {task.buttonText}
+            </button>
+            <button
+              onClick={() => dispatch({ type: "delete", payload: index })}
+              disabled={!task.isChecked}
+            >
+              Delete
+            </button>
+            <input
+              type="checkbox"
+              name={index}
+              onChange={() => handleCheckbox(index)}
+            ></input>
           </li>
-
         ))}
-              
       </ul>
     </>
   );
--- src/components/TaskUpdate.jsx
+++ src/components/TaskUpdate.jsx
@@ -1,50 +1,71 @@
 function TaskUpdate(state, action) {
   switch (action.type) {
     case "input":
-        console.log("state.input" + JSON.stringify(state));
       return { ...state, initialInput: action.payload };
     case "add":
-      console.log("add");
-      console.log("state.initialInput" + JSON.stringify(state));
       return {
         ...state,
-        tasks: [...state.tasks, { text: state.initialInput, isChecked: false,isEditClicked:true }],
+        tasks: [
+          ...state.tasks,
+          {
+            text: state.initialInput,
+            isChecked: false,
+            isEditClicked: true,
+            buttonText: "Edit",
+          },
+        ],
         initialInput: "",
       };
     case "edit":
-      console.log("edit");
-
       return {
         ...state,
-        tasks: state.tasks.map((editTask,index) => {
-            if(index ===action.payload){
-                return{...editTask,isEditClicked:!editTask.isEditClicked}
-            }
-            else
-            { return editTask}
-        })
-
-      }
+        tasks: state.tasks.map((editTask, index) => {
+          if (index === action.payload) {
+            return {
+              ...editTask,
+              isEditClicked: !editTask.isEditClicked,
+              buttonText: "Edit",
+            };
+          } else {
+            return editTask;
+          }
+        }),
+      };
+    case "edit-task":
+      return {
+        ...state,
+        tasks: state.tasks.map((currentTask, index) => {
+          if (action.payload.index === index) {
+            return {
+              ...currentTask,
+              text: action.payload.value,
+              buttonText: "Save",
+            };
+          } else {
+            return currentTask;
+          }
+        }),
+      };
     case "delete":
-      console.log("delete");
-      return state;
+        const tempTasks = [...state.tasks]; 
+        tempTasks.splice(action.payload, 1);
+        return {
+          ...state,
+          tasks: tempTasks
+        };
     case "toggle-task":
       return {
         ...state,
         tasks: state.tasks.map((task, index) => {
-            if(index === action.payload){
-                return{...task,isChecked: !task.isChecked};
-            }
-            else {
-                return task;
-            }
-        }
-                 
-        ),
+          if (index === action.payload) {
+            return { ...task, isChecked: !task.isChecked };
+          } else {
+            return task;
+          }
+        }),
       };
     default:
       return state;
   }
 }
-
 export default TaskUpdate;
